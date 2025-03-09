7fdbd3794ea2e2835975ab87ce705a931cb5c89c
barezzali2 <barezrealy22@gmail.com>
2025-03-09 23:59:43 +0300

    endpoints

--- main.js
+++ main.js
@@ -5,6 +5,26 @@ const port = 3003;
 const cors = require('cors');
 app.use(cors());
 
+// Home
+app.get('/', (req, res) => {
+
+});
+
+// Product List
+app.get('/', (req, res) => {
+
+});
+
+// Search
+app.get('/', (req, res) => {
+
+});
+
+// Shopping List
+app.get('/', (req, res) => {
+
+});
+
 
 app.listen(port, () => {
     console.log(`This application is listening on port ${port}`);
